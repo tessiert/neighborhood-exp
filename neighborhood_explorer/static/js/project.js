caeee150858df75fa1f7f3124f34de9247843e44
Trace <trace.tessier@gmail.com>
2023-05-25 18:54:06 -0400

    Added validation for POI search

--- neighborhood_explorer/static/js/project.js
+++ neighborhood_explorer/static/js/project.js
@@ -1,7 +1,31 @@
 /* Project specific Javascript goes here. */
+
+// Change active menu tab
 $(document).ready(function () {
   $('li.active').removeClass('active');
   $('a[href="' + location.pathname + '"]')
     .closest('li')
     .addClass('active');
+});
+
+// Require valid search criteria to enable 'Submit' button for POI search
+$(document).ready(function() {
+  $('.poi_menu').on('input change', function() {
+    if(!$('#point_of_interest').val() || !$('#radius').val() ||
+      !$('#sort_method').val()) {
+        $('#poi_submit').prop('disabled', true);        
+      }
+    else {
+      $('#poi_submit').prop('disabled', false);
+    }
+    // alert($('#point_of_interest').val());
+    // if(
+    //   ($('#point_of_interest').val() != "") && 
+    //   ($('#radius').val() != "") &&
+    //   ($('#sort_method').val() != "")) {
+    //     $('#poi_submit').prop('disabled', false);
+    // } else {
+    //     $('#poi_submit').prop('disabled', true);
+    // }
+  });
 });
