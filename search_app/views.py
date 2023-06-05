8eec7ba8fd8a2088a72251f43a1ad5bce689eafa
Trace <trace.tessier@gmail.com>
2023-06-05 19:11:19 -0400

    Refactor search_app/views.py

--- search_app/views.py
+++ search_app/views.py
@@ -11,18 +11,92 @@ from config.settings.base import MAP_KEY, WEATHER_KEY
 # Select appropriate map zoom level based on search radius (in meters)
 map_zoom = {"3219": "12", "8047": "11", "16093": "10", "40234": "9"}
 
+def poi_search(request):
+    # If declutter is checked, it will return a value, otherwise no
+    # return value.  Input to mapper must be a 'boolean' text string.
+    if request.POST.get("declutter"):
+        declutter = "true"
+    else:
+        declutter = "false"
+    request.session["declutter"] = declutter
+    
+    # Since user is doing a POI search, we've already pulled the geolocation
+    # data.
+    address = request.POST.get("address")
+    latitude = request.POST.get("latitude")
+    longitude = request.POST.get("longitude")
+    #            display_order = request.POST.get('display_order')
+    request.session["point_of_interest"] = request.POST.get("point_of_interest")
+    request.session["radius"] = request.POST.get("radius")
+    request.session["sort_method"] = request.POST.get("sort_method")
+
+    places_URL = "https://www.mapquestapi.com/search/v4/place?key={map_key}&circle={lon},{lat},{rad}&q={search_term}&sort={sort_method}".format(
+        map_key=MAP_KEY,
+        lat=latitude,
+        lon=longitude,
+        rad=request.session["radius"],
+        search_term=request.session["point_of_interest"],
+        sort_method=request.session["sort_method"],
+    )
+
+    try:
+        places_response = requests.get(places_URL).json()
+    except requests.ConnectionError:
+        context={"server": "Mapquest points of interest"}
+        return (request, "500.html", context, 500)
+
+    poi_info = []
+    # Initialize with marker for home location
+    place_markers = f"locations={latitude},{longitude}|via-sm-green||"
+    count = 1
+    for place in places_response["results"]:
+        poi_description = place["displayString"] + "\n\n"
+        poi_link = place["displayString"].replace(" ", "%20").replace(",", "%2C")
+        poi_info.append({"description": poi_description, "link": poi_link})
+        place_markers += (
+            str(place["place"]["geometry"]["coordinates"][1])
+            + ","
+            + str(place["place"]["geometry"]["coordinates"][0])
+            + "|"
+            + "marker-sm-red-"
+            + str(count)
+            + "||"
+        )
+        count += 1
+    place_markers = place_markers.rstrip("|")
+
+    if not places_response["results"]:
+        poi_info = [{"description": "No results found within the current search radius.", "link": ""}]
+
+
+    map_URL = "https://www.mapquestapi.com/staticmap/v5/map?key={map_key}&center={lat},{lon}&size=260,200@2x&scalebar=true&&zoom={zoom}&declutter={declutter}&{places}".format(
+        map_key=MAP_KEY,
+        lat=latitude,
+        lon=longitude,
+        zoom=map_zoom[request.session["radius"]],
+        declutter=request.session["declutter"],
+        places=place_markers,
+    )
+
+    context = {
+        "formatted_address": address,
+        "latitude": latitude,
+        "longitude": longitude,
+        "temperature": request.POST.get("temperature"),
+        "summary": request.POST.get("summary"),
+        "days": request.session["days"],
+        "map_url": map_URL,
+        "poi_info": poi_info,
+        "poi_start_val": request.session["point_of_interest"],
+        "radius_start_val": request.session["radius"],
+        "sort_start_val": request.session["sort_method"],
+        "declutter_start_val": request.session["declutter"],
+        "anchor": "poi_anchor",
+    }
+    return (request, "pages/update.html", context, 200)
+
 class UpdateView(View):
     def post(self, request):
-             
-        # Since user is doing a POI search, we've already pulled the geolocation
-        # data.
-        address = request.POST.get("address")
-        latitude = request.POST.get("latitude")
-        longitude = request.POST.get("longitude")
-        #            display_order = request.POST.get('display_order')
-        request.session["point_of_interest"] = request.POST.get("point_of_interest")
-        request.session["radius"] = request.POST.get("radius")
-        request.session["sort_method"] = request.POST.get("sort_method")
         # If declutter is checked, it will return a value, otherwise no
         # return value.  Input to mapper must be a 'boolean' text string.
         if request.POST.get("declutter"):
@@ -30,71 +104,12 @@ class UpdateView(View):
         else:
             declutter = "false"
         request.session["declutter"] = declutter
+            
+        # Since we are updating, it must be a POI search
+        request, template, context, status = poi_search(request)
+        
+        return render(request, template_name=template, context=context, status=status)
 
-        places_URL = "https://www.mapquestapi.com/search/v4/place?key={map_key}&circle={lon},{lat},{rad}&q={search_term}&sort={sort_method}".format(
-            map_key=MAP_KEY,
-            lat=latitude,
-            lon=longitude,
-            rad=request.session["radius"],
-            search_term=request.session["point_of_interest"],
-            sort_method=request.session["sort_method"],
-        )
-
-        try:
-            places_response = requests.get(places_URL).json()
-        except requests.ConnectionError:
-            return render(
-                request, template_name="500.html", context={"server": "Mapquest points of interest"}, status=500
-            )
-
-        poi_info = []
-        # Initialize with marker for home location
-        place_markers = f"locations={latitude},{longitude}|via-sm-green||"
-        count = 1
-        for place in places_response["results"]:
-            poi_description = place["displayString"] + "\n\n"
-            poi_link = place["displayString"].replace(" ", "%20").replace(",", "%2C")
-            poi_info.append({"description": poi_description, "link": poi_link})
-            place_markers += (
-                str(place["place"]["geometry"]["coordinates"][1])
-                + ","
-                + str(place["place"]["geometry"]["coordinates"][0])
-                + "|"
-                + "marker-sm-red-"
-                + str(count)
-                + "||"
-            )
-            count += 1
-        place_markers = place_markers.rstrip("|")
-
-        if not places_response["results"]:
-            poi_info = [{"description": "No results found within the current search radius.", "link": ""}]
-
-        map_URL = "https://www.mapquestapi.com/staticmap/v5/map?key={map_key}&center={lat},{lon}&size=260,200@2x&scalebar=true&&zoom={zoom}&declutter={declutter}&{places}".format(
-            map_key=MAP_KEY,
-            lat=latitude,
-            lon=longitude,
-            zoom=map_zoom[request.session["radius"]],
-            declutter=declutter,
-            places=place_markers,
-        )
-
-        context = {
-            "formatted_address": address,
-            "latitude": latitude,
-            "longitude": longitude,
-            "temperature": request.POST.get("temperature"),
-            "summary": request.POST.get("summary"),
-            "days": request.session["days"],
-            "map_url": map_URL,
-            "poi_info": poi_info,
-            "poi_start_val": request.session["point_of_interest"],
-            "radius_start_val": request.session["radius"],
-            "sort_start_val": request.session["sort_method"],
-            "declutter_start_val": request.session["declutter"],
-            "anchor": "poi_anchor",
-        }
-        return render(request, template_name="pages/update.html", context=context)
     
 class SearchView(View):
     @classmethod
@@ -124,90 +139,10 @@ class SearchView(View):
         DAY_FORMAT = "%A,"
         DATE_FORMAT = "%b. %d"
         
-        # If declutter is checked, it will return a value, otherwise no
-        # return value.  Input to mapper must be a 'boolean' text string.
-        if request.POST.get("declutter"):
-            declutter = "true"
-        else:
-            declutter = "false"
-        request.session["declutter"] = declutter
-
-        # If user is doing a POI search, we've already pulled the geolocation
-        # data.
         if request.POST.get("poi_search"):
-            address = request.POST.get("address")
-            latitude = request.POST.get("latitude")
-            longitude = request.POST.get("longitude")
-            #            display_order = request.POST.get('display_order')
-            request.session["point_of_interest"] = request.POST.get("point_of_interest")
-            request.session["radius"] = request.POST.get("radius")
-            request.session["sort_method"] = request.POST.get("sort_method")
-
-            places_URL = "https://www.mapquestapi.com/search/v4/place?key={map_key}&circle={lon},{lat},{rad}&q={search_term}&sort={sort_method}".format(
-                map_key=MAP_KEY,
-                lat=latitude,
-                lon=longitude,
-                rad=request.session["radius"],
-                search_term=request.session["point_of_interest"],
-                sort_method=request.session["sort_method"],
-            )
-
-            try:
-                places_response = requests.get(places_URL).json()
-            except requests.ConnectionError:
-                return render(
-                    request, template_name="500.html", context={"server": "Mapquest points of interest"}, status=500
-                )
-
-            poi_info = []
-            # Initialize with marker for home location
-            place_markers = f"locations={latitude},{longitude}|via-sm-green||"
-            count = 1
-            for place in places_response["results"]:
-                poi_description = place["displayString"] + "\n\n"
-                poi_link = place["displayString"].replace(" ", "%20").replace(",", "%2C")
-                poi_info.append({"description": poi_description, "link": poi_link})
-                place_markers += (
-                    str(place["place"]["geometry"]["coordinates"][1])
-                    + ","
-                    + str(place["place"]["geometry"]["coordinates"][0])
-                    + "|"
-                    + "marker-sm-red-"
-                    + str(count)
-                    + "||"
-                )
-                count += 1
-            place_markers = place_markers.rstrip("|")
-
-            if not places_response["results"]:
-                poi_info = [{"description": "No results found within the current search radius.", "link": ""}]
-
-
-            map_URL = "https://www.mapquestapi.com/staticmap/v5/map?key={map_key}&center={lat},{lon}&size=260,200@2x&scalebar=true&&zoom={zoom}&declutter={declutter}&{places}".format(
-                map_key=MAP_KEY,
-                lat=latitude,
-                lon=longitude,
-                zoom=map_zoom[request.session["radius"]],
-                declutter=declutter,
-                places=place_markers,
-            )
-
-            context = {
-                "formatted_address": address,
-                "latitude": latitude,
-                "longitude": longitude,
-                "temperature": request.POST.get("temperature"),
-                "summary": request.POST.get("summary"),
-                "days": request.session["days"],
-                "map_url": map_URL,
-                "poi_info": poi_info,
-                "poi_start_val": request.session["point_of_interest"],
-                "radius_start_val": request.session["radius"],
-                "sort_start_val": request.session["sort_method"],
-                "declutter_start_val": request.session["declutter"],
-                "anchor": "poi_anchor",
-            }
-            return render(request, template_name="pages/search.html", context=context)
+            request, template, context, status = poi_search(request)
+                      
+            return render(request, template_name=template, context=context, status=status)
 
         # If address is supplied, then user clicked a 'Top Searches' link.
         # If not, get address from posted search bar form data.
